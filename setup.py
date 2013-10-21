638f37314a1d3ffd27aee2992b7e6395d84daaf6
Åsmund Grammeltvedt <asmundg@snap.tv>
2013-10-21 21:47:48 +0200

    Mention dependencies

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import find_packages, setup
 
 setup(name='csv2ofx',
       packages=find_packages(),
+      install_requires=['lxml'],
       entry_points={
           'console_scripts': [
               'csv2ofx-fokus = csv2ofx.fokus:main']})
